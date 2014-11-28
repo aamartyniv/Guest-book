353762222efdea76ae945933bab87fdd9d3c67e1
Alexandr Martyniv <aamartyniv@gmail.com>
2014-11-28 22:44:47 +0200

    registration

--- view/registration.php
+++ view/registration.php
@@ -1,7 +1,40 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: igor
- * Date: 28.11.14
- * Time: 20:01
- */ 
+<div class="col-xs-2 col-md-2>
+
+    <form role="form" name="registration" metod="post" action="registration.php">
+       <div class="form-group">
+           <label class="col-sm-2" for="name"> name </label>
+
+           <div class="col-xs-2">
+               <input type="text" class="form-control" id="name" placeholder="name">
+           </div>
+
+       </div>
+
+<div class="form-group">
+    <label class="col-sm-2" for="email"> email </label>
+
+    <div class="col-xs-2">
+        <input type="email" class="form-control" id="email" placeholder="email">
+    </div>
+</div>
+
+<div class="form-group">
+    <label class="col-sm-2" for="password"> password </label>
+
+    <div class="col-xs-2">
+        <input type="password" class="form-control" id="password" maxlength="20" placeholder="password">
+    </div>
+</div>
+
+<div class="form-group">
+    <div class="col-sm-offset-2">
+        <button type="submit" class="btn btn-primary btn-sm"> registration </button>
+    </div>
+</div>
+
+</form>
+
+</div>
+
+
+
