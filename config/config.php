04af39cf3dfca030c5798be56169c0e2c448f09e
Igor Dmitrencko <iadmitrencko@gmail.com>
2014-11-28 15:18:23 +0200

    config

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+// in fields 'db.name', 'db.user' and 'db.password' you must enter your data
+// but field 'db.name' must be one for you and me, you must create in folder 'sql' file 'create_db' and here db name should be ))
+
+return [
+    'db.name' => '',
+    'db.user' => '',
+    'db.host' => 'localhost',
+    'db.password' => ''
+];
