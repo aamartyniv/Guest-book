35927cf239604be52c6701197c6f384192c84820
Igor Dmitrencko <iadmitrencko@gmail.com>
2014-11-28 18:13:12 +0200

    aaadddd

--- config/config.php
+++ config/config.php
@@ -1,8 +1,4 @@
 <?php
-
-// in fields 'db.name', 'db.user' and 'db.password' you must enter your data
-// but field 'db.name' must be one for you and me, you must create in folder 'sql' file 'create_db' and here db name should be ))
-
 return [
     'db.name' => 'fbase',
     'db.user' => 'admin',
