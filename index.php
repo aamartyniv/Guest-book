81a471adf1fa59965773e8340b013fd8af80a9c2
Igor Dmitrencko <iadmitrencko@gmail.com>
2014-11-28 20:03:26 +0200

    create view

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-<<<<<<< HEAD
-
-printf ("fack general");
-=======
 error_reporting( E_ALL );
 
 include_once "core/route.php";
->>>>>>> 2c6150556c669f11ae13afdf7785fc9bd0f72904
---
+++ view/registration.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: igor
+ * Date: 28.11.14
+ * Time: 20:01
+ */ 
