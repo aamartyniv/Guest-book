3be7384f638b68a5e02bdbc2f44703653f67fd56
Alexandr Martyniv <aamartyniv@gmail.com>
2014-11-28 20:19:46 +0200

    base

--- sql/create_table_registration_data.sql
+++ sql/create_table_registration_data.sql
@@ -1,6 +1,6 @@
-create table registration_data (
-'id' int(10) primary key auto_increment,
-'name' varchar(255) not null,
-'password' varchar(100) not null,
-'email' varchar(255) key not null
-)engine=InnoDB default charset=utf8;
+CREATE TABLE  'registration_data' (
+'id' int(10) PRIMARY KEY AUTO_INCREMENT,
+'name' varchar(255) NOT NULL,
+'password' varchar(100) NOT NULL,
+'email' varchar(255) key NOT null
+)ENGINE=InnoDB CHARSET=utf8;
